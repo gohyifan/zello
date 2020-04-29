f7e595ca57aa48c3faaa4baf436102b639bf16fa
Yi Fan Goh <gohyf91@gmail.com>
2020-04-29 22:56:31 +0800

    App structure completed

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">
+        Zello
+      </div>
+    </div>
+  );
+}
+
+export default App;
