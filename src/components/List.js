ad2f5c924961923692daf652d2d1f2d81b8a2029
Yi Fan Goh <gohyf91@gmail.com>
2020-04-30 14:13:23 +0800

    add a card function completed

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,36 @@
-import "../styles/List.css";
-
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import shortid from "shortid";
 
+// relative imports
+import "../styles/List.css";
 import Card from "./Card";
+import CardEditor from "./CardEditor";
 
 class List extends Component {
+  state = {
+    addingCard: false
+  };
+
+  toggleAddingCard = () =>
+    this.setState({ addingCard: !this.state.addingCard });
+
+  addCard = async cardText => {
+    const { listId, dispatch } = this.props;
+
+    this.toggleAddingCard();
+
+    const cardId = shortid.generate();
+
+    dispatch({
+      type: "ADD_CARD",
+      payload: { cardText, cardId, listId }
+    });
+  };
+
   render() {
     const { list } = this.props;
+    const { addingCard } = this.state;
 
     return (
       <div className="List">
@@ -24,6 +47,17 @@ class List extends Component {
               listId={list._id}
             />
           ))}
+          {addingCard ? (
+            <CardEditor
+              onSave={this.addCard}
+              onCancel={this.toggleAddingCard}
+              adding
+            />
+          ) : (
+            <div className="Toggle-Add-Card" onClick={this.toggleAddingCard}>
+              <ion-icon name="add" /> Add a card
+            </div>
+          )}
       </div>
     );
   }
