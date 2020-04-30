5896ac8e39bc2023ed99d0a16af0a59757398635
Yi Fan Goh <gohyf91@gmail.com>
2020-04-30 17:45:48 +0800

    Added AddList component to Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,19 +1,38 @@
-import "../styles/Board.css";
-
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import "../styles/Board.css";
 import List from "./List";
+import AddList from "./AddList";
 
 class Board extends Component {
+  state = {
+    addingList: false
+  };
+
+  toggleAddingList = () => this.setState({ addingList: !this.state.addingList });
+
+
   render() {
     const { board } = this.props;
+    const { addingList } = this.state;
 
     return (
-      <div className="Board">
-        {board.lists.map((listId, index) => {
-          return <List listId={listId} key={listId} index={index} />;
-        })}
+      <div>
+        <div className="Board">
+          {board.lists.map((listId, index) => {
+            return <List listId={listId} key={listId} index={index} />;
+          })}
+        </div>
+        <div className="Add-List">
+          {addingList ? (
+            <AddList toggleAddingList={this.toggleAddingList} />
+          ) : (
+            <div onClick={this.toggleAddingList} className="Add-List-Button">
+              <ion-icon name="add" /> Add a list
+            </div>
+          )}
+        </div>
       </div>
     );
   }
